06ef3f7601593258213b738d8219e4c0bb2eb0d1
Ali Raza Ramejo <alirazakhan2540@gmail.com>
2024-02-17 10:10:46 +0500

    edit make profile

--- src/Components/pages/Home/Search.jsx
+++ src/Components/pages/Home/Search.jsx
@@ -1,28 +1,31 @@
-import React from 'react'
-import Modal from '../../../utils/Modal'
+import React from "react";
+import Modal from "../../../utils/Modal";
 import { CiSearch } from "react-icons/ci";
-function Search({modal, setModal}) {
-  
+function Search({ modal, setModal }) {
   return (
     <>
-    <Modal modal={modal} setModal={setModal}>
-      <div className={`absolute sm:relative right-4 left-4 top-[4rem] sm:left-0 sm:top-0 ${modal ? "visible opacity-100 " : "invisible sm:visible opacity-100 opacity-0"} transition-all duration-100`}>
-        <div className="flex items-center gap-1 bg-gray-100 px-2 rounded-full relative z-10">
-        <span className="text-2xl text-gray-400">
+      <Modal modal={modal} setModal={setModal}>
+        <div
+          className={`absolute sm:relative right-4 left-4 top-[4rem] sm:left-0 sm:top-0 ${
+            modal
+              ? "visible opacity-100 "
+              : "invisible sm:visible opacity-100 opacity-0"
+          } transition-all duration-100`}
+        >
+          <div className="flex items-center gap-1 bg-gray-100 px-2 rounded-full relative z-10">
+            <span className="text-2xl text-gray-400">
               <CiSearch />
             </span>
             <input
-              
-              
               className="bg-transparent outline-none py-[0.7rem] text-sm w-full"
               type="text"
               placeholder="Search Medium"
             />
+          </div>
         </div>
-      </div>
-    </Modal>
+      </Modal>
     </>
-  )
+  );
 }
 
-export default Search
+export default Search;
